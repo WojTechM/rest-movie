a4b5995fecdbefb7eb8f0de5d1a2a2101bd1a80a
WojTechM <wojciech.m1997@gmail.com>
2018-08-02 13:43:23 +0200

    Refactor MovieRepository - updated way of persising pornstars

--- src/main/java/com/codecool/krk/repositories/MovieRepository.java
+++ src/main/java/com/codecool/krk/repositories/MovieRepository.java
@@ -4,6 +4,7 @@ import com.codecool.krk.model.Movie;
 import com.codecool.krk.model.Pornstar;
 
 import javax.persistence.EntityTransaction;
+import javax.persistence.RollbackException;
 import java.util.List;
 
 public class MovieRepository extends Repository <Movie> {
@@ -15,12 +16,30 @@ public class MovieRepository extends Repository <Movie> {
     @Override
     public void update(Movie entity) {
         List<Pornstar> pornstars = entity.getPornstars();
+        savePornstars(pornstars);
         EntityTransaction transaction = em.getTransaction();
-        transaction.begin();
-        for(Pornstar pornstar : pornstars) {
-            em.merge(pornstar);
+        try {
+            transaction.begin();
+            em.merge(entity);
+            transaction.commit();
+        } catch (RollbackException e) {
+            transaction.rollback();
         }
-        em.merge(entity);
-        transaction.commit();
+    }
+
+    private void savePornstars(List<Pornstar> pornstars) {
+        EntityTransaction transaction = em.getTransaction();
+        for(Pornstar pornstar : pornstars) {
+            try {
+                transaction.begin();
+                em.persist(pornstar);
+                transaction.commit();
+            } catch (Exception e) {
+                transaction.rollback();
+                transaction.begin();
+                em.merge(pornstar);
+                transaction.commit();
+            }
+        }
     }
 }
